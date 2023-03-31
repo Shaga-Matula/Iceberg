1a0ea3a8c562e26fb1518ea413e17157ff2c9ee7
Paul-Gleeson <Shagamatula@gmail.com>
2023-03-31 19:25:42 +0000

    test connection read write to sheets

--- run.py
+++ run.py
@@ -14,6 +14,31 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("Iceberg")
 
-grids = SHEET.worksheet('grid')
-data = grids.get_all_values()
-print(data)
+grids_sheet = SHEET.worksheet('grid')
+data = grids_sheet.get_all_values()
+
+
+# def make_playbord_in_sheet(data):
+#     """ 
+#     Create a board for the game in ICEBERG sheet
+#     """ 
+
+
+# for cell in grids_sheet.range('a1:b2'):
+#     print(cell.value)
+
+# print(grids_sheet.acell('b1').value)
+# print(grids_sheet.cell(1,1).value)
+
+i = 1
+o = 'b'
+b = o,i
+c = str(b)
+print(c)
+
+
+# while i  <= 18:
+#     grids_sheet.update_acell(('A4'),'30000000')
+#     i += i
+
+# grids.write(1, 0, 'ISBT DEHRADUN')
