f5504fd2228987acd5640a59b477f79df3f9c69b
Shaga-Matula <Shagamatula@gmail.com>
2023-04-12 15:01:41 +0000

    testing for heroku issues with deployment

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-import gspread
-from google.oauth2.service_account import Credentials
+# import gspread
+# from google.oauth2.service_account import Credentials
 import json
 import os
 from colorama import Fore, Back, Style
@@ -14,20 +14,20 @@ import string
 #import sys for break in loops
 import sys
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-]
+# SCOPE = [
+#     "https://www.googleapis.com/auth/spreadsheets",
+#     "https://www.googleapis.com/auth/drive.file",
+#     "https://www.googleapis.com/auth/drive"
+# ]
 
-creds = json.load(open('creds.json'))
-CREDS = Credentials.from_service_account_info(creds)
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("Iceberg")
+# creds = json.load(open('creds.json'))
+# CREDS = Credentials.from_service_account_info(creds)
+# SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+# GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# SHEET = GSPREAD_CLIENT.open("Iceberg")
 
-grids_sheet = SHEET.worksheet('grid')
-data = grids_sheet.get_all_values()
+# grids_sheet = SHEET.worksheet('grid')
+# data = grids_sheet.get_all_values()
 
 very_near = []
 print(f"Very near here = {very_near}")
